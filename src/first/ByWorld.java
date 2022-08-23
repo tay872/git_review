a5ef8174a4b12e3da2661f843939dcdd6445cfa9
tay872 <107067082+tay872@users.noreply.github.com>
2022-08-23 11:31:16 -0400

    ByWorld.java is created

---
+++ src/first/ByWorld.java
@@ -0,0 +1,9 @@
+package first;
+
+public class ByWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Maybe 5 week of java left");
+        System.out.println("1.5 weeks of git");
+    }
+}
