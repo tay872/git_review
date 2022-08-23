16da4a61f7fe888486d687312c56a0d9b5857804
tay872 <107067082+tay872@users.noreply.github.com>
2022-08-23 12:48:36 -0400

    Create Test.java

---
+++ src/first/Test.java
@@ -0,0 +1,6 @@
+public class TestClass {
+    public static void main(String[] args) {
+
+        System.out.println("Maybe 5 week of java left");
+        System.out.println("1.5 weeks of git");
+    }
