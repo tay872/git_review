3c3777ca23ed3c29b80324e8fc9eae7c3f97a021
tay872 <107067082+tay872@users.noreply.github.com>
2022-08-24 20:00:49 -0400

    Create retest.java

---
+++ src/first/retest.java
@@ -0,0 +1,7 @@
+public class ByWorld {
+    public static void main(String[] args) {
+
+       // System.out.println("Maybe 5 week of java left");
+       // System.out.println("1.5 weeks of git");
+    }
+}
